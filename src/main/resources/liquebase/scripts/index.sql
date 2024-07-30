e9aff6745d62ea3708a1df91d691666d5c5568c6
Mariia <mariia_merkel@mail.ru>
2024-07-30 14:45:17 +0300

    create script for creat table

---
+++ src/main/resources/liquebase/scripts/index.sql
@@ -0,0 +1,10 @@
+-- liquibase formatted sql
+
+-- changeset mariiam:1
+
+CREATE TABLE notification_task (
+	id bigserial PRIMARY KEY,
+	chat_id int8 NOT NULL,
+	message VARCHAR(255) NOT NULL,
+	date_time datetime NOT NULL
+);
