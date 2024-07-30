ad0e4c1208bdf7a4037e8d34441f64a4f17b5e21
Mariia <mariia_merkel@mail.ru>
2024-07-30 14:44:46 +0300

    create model for notification

--- src/main/java/pro/sky/telegrambot/entity/NotificationTask.java
+++ src/main/java/pro/sky/telegrambot/entity/NotificationTask.java
@@ -1,2 +1,33 @@
-package pro.sky.telegrambot.entity;public class NotificationTask {
+package pro.sky.telegrambot.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Entity
+public class NotificationTask {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer chat_id;
+    private String message;
+    private LocalDate datetime;
+
+    public NotificationTask() {
+    }
+
+    public NotificationTask(Long id, Integer chat_id, String message, LocalDate datetime) {
+        this.id = id;
+        this.chat_id = chat_id;
+        this.message = message;
+        this.datetime = datetime;
+    }
 }
