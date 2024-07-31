87634eecffbf6d3e976e4c574d7f5ba6c52cc11d
MariiaMerkel <mariia_merkel@mail.ru>
2024-07-31 09:42:18 +0300

    добавила миграцию

--- src/main/resources/liquibase/scripts/index.sql
+++ src/main/resources/liquibase/scripts/index.sql
@@ -6,5 +6,5 @@ CREATE TABLE notification_task (
 	id bigserial PRIMARY KEY,
 	chat_id int8 NOT NULL,
 	message VARCHAR(255) NOT NULL,
-	date_time timestamptz NOT NULL
+	date_time timestamp NOT NULL
 );
