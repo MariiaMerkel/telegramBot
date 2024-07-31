41150c510842ffc94553201c66c5f8694bd2ebd9
MariiaMerkel <mariia_merkel@mail.ru>
2024-07-31 16:39:24 +0300

    implemented adding reminders to the repository

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -2,23 +2,31 @@ package pro.sky.telegrambot.listener;
 
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.request.SendMessage;
-import com.pengrad.telegrambot.response.SendResponse;
+import jakarta.annotation.PostConstruct;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import jakarta.annotation.PostConstruct;
-import java.util.List;
+import pro.sky.telegrambot.entity.NotificationTask;
+import pro.sky.telegrambot.exception.CustomNotificationTaskException;
+import pro.sky.telegrambot.service.NotificationTaskService;
 
 @Service
 public class TelegramBotUpdatesListener implements UpdatesListener {
 
-    private Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
+    private final Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
+    private final NotificationTaskService notificationTaskService;
+    private final TelegramBot telegramBot;
 
-    @Autowired
-    private TelegramBot telegramBot;
+    public TelegramBotUpdatesListener(NotificationTaskService notificationTaskService, TelegramBot telegramBot) {
+        this.notificationTaskService = notificationTaskService;
+        this.telegramBot = telegramBot;
+    }
+
 
     @PostConstruct
     public void init() {
@@ -28,11 +36,29 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     @Override
     public int process(List<Update> updates) {
         updates.forEach(update -> {
-            logger.info("Processing update: {}", update);
-            SendMessage message = new SendMessage(update.message().chat().id(), "messageText");
-            SendResponse response = telegramBot.execute(message);
+            Message message = update.message();
+            try {
+                NotificationTask notificationTask = notificationTaskService.add(message);
+                sendMessage(notificationTask);
+            } catch (CustomNotificationTaskException e) {
+                sendMessage(message.chat().id(), e.getMessage());
+            }
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
 
+    private void sendMessage(NotificationTask notificationTask) {
+        String sendMessageText = String.format(
+                "Задача была добавлена в расписание, и я напомню %s в %s",
+                notificationTask.getMessage(),
+                notificationTask.getDateTime().
+                                format(DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm")));
+        SendMessage sendMessage = new SendMessage(notificationTask.getChat_id(), sendMessageText);
+        telegramBot.execute(sendMessage);
+    }
+
+    private void sendMessage(Long chat_id, String exception) {
+        SendMessage sendMessage = new SendMessage(chat_id, exception);
+        telegramBot.execute(sendMessage);
+    }
 }
--- src/main/java/pro/sky/telegrambot/service/NotificationTaskService.java
+++ src/main/java/pro/sky/telegrambot/service/NotificationTaskService.java
@@ -1,2 +1,10 @@
-package pro.sky.telegrambot.service;public interface NotificationTaskService {
+package pro.sky.telegrambot.service;
+
+import com.pengrad.telegrambot.model.Message;
+import pro.sky.telegrambot.entity.NotificationTask;
+
+public interface NotificationTaskService {
+
+    NotificationTask add(Message message);
+
 }
